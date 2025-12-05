17f4964622d91b2e2605f4bb44453a843fdeeb18
SIFU <13341091218@189.cn>
2025-12-05 14:27:36 +0800

    Update perf_report.py

--- 2025.12.16/Samples/perf_report.py
+++ 2025.12.16/Samples/perf_report.py
@@ -13,7 +13,7 @@ oot_dat = pd.read_excel('../../Data/data_01/oot_data/oot.xlsx')
 train_X = train_dat.loc[:,~train_dat.columns.isin(['y','data_category'])]
 valid_X = valid_dat.loc[:,valid_dat.columns.isin(['y','data_category'])]
 
-#仅仅为了生成一个模型，可以替换成任何一个用户自己的模型
+#Merely for the purpose of generating a model, it can be replaced with any user's own model.
 from rascpy.Tree import auto_xgb
 perf_cands,params_cands,clf_cands,vars_cands = auto_xgb(train_X,train_dat.y,valid_X,valid_dat.y,train_w=train_dat.sample_weight,val_w=valid_dat.sample_weight,metric='auc',cost_time=60*5,cands_num=5,variance_level=1)
 clf = clf_cands[0]
@@ -27,3 +27,4 @@ dats['train']=(train_dat.y,train_hat,train_dat.sample_weight)
 dats['val']=(valid_dat.y,valid_hat,valid_dat.sample_weight)
 # (datas,target_label=None,cut_data_name=None,wide=0.05,thin=0.01,thin_head=10,lift=None,score_reverse=True,writer=None,sheet_name=lan['Performance of the model'],filePath=None):
 _=write_performance(dats,lift=(5,10,20),wide=0.1,thin=None,filePath='./perf_report.xlsx')
+
